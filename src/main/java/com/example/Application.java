abb870fe5e40f6cf2c900a1b87692b77af8c2020
Yaroslav <jarlua11@gmail.com>
2021-06-12 12:21:25 +0300

    Some minor changes

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,25 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
-//        JobLogger bean = run.getBean(JobLogger.class);
-//        JobLog jobLog = JobLog.builder()
-//                .logLevel(JobLogLevel.INFO)
-//                .jobKey(new JobKey("jobId", "jobGroupName").toString())
-//                .triggerKey(new TriggerKey("triggerId", "triggerGroupName").toString())
-//                .errorMessage("Job successfully scheduled")
-//                .build();
-//
-//        bean.log(jobLog);
-
-//        JobLogger bean = run.getBean(JobLogger.class);
-//        List<JobLog> allLogs = bean.findLogsByParameters();
-//        allLogs.forEach(l -> System.out.println(l));
+        SpringApplication.run(Application.class, args);
     }
 }
